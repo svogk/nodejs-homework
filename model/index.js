18ddad8352ffa2ab394b3ce8747971f5acaae872
Svitlana Bondarenko <svogk@ua.fm>
2021-02-24 16:59:35 +0200

    add hw-02

--- model/index.js
+++ model/index.js
@@ -1,15 +1,66 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require("fs/promises");
+const { v4: uuid } = require("uuid");
 
-const listContacts = async () => {}
+const path = require("path");
+const contactsPath = path.resolve(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const allContacts = await fs.readFile(contactsPath, "utf8");
+    return JSON.parse(allContacts);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const allContacts = await listContacts();
+    return allContacts.find((contact) => contact.id.toString() === contactId);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  try {
+    const allContacts = await listContacts();
+    const newListContacts = allContacts.filter(
+      (contact) => contact.id !== contactId
+    );
+    fs.writeFile(contactsPath, JSON.stringify(newListContacts));
+    return listContacts();
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (body) => {
+  try {
+    const allContacts = await listContacts();
+    const newContact = { id: uuid(), ...body };
+    allContacts.push(newContact);
+    fs.writeFile(contactsPath, JSON.stringify(allContacts));
+    return newContact;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const updateContact = async (contactId, body) => {
+  try {
+    const allContacts = await listContacts();
+    const index = allContacts.findIndex(
+      (contact) => contact.id.toString() === contactId
+    );
+    if (index === -1) return;
+    allContacts[index] = { ...allContacts[index], ...body };
+    fs.writeFile(contactsPath, JSON.stringify(allContacts));
+    return allContacts[index];
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 module.exports = {
   listContacts,
@@ -17,4 +68,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
---
+++ routes/api/validation.js
@@ -0,0 +1,43 @@
+const Joi = require("joi");
+
+const schemaAddContact = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  phone: Joi.string().required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+});
+
+const schemaUpdateContact = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).optional(),
+  phone: Joi.string().optional(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .optional(),
+}).min(1);
+
+const validate = (schema, obj, next) => {
+  const { error } = schema.validate(obj);
+  if (error) {
+    const [{ message }] = error.details;
+    return next({
+      status: 400,
+      message: `Filed: ${message.replace(/"/g, "")}`,
+    });
+  }
+  next();
+};
+
+module.exports.addContact = (req, res, next) => {
+  return validate(schemaAddContact, req.body, next);
+};
+
+module.exports.updateContact = (req, res, next) => {
+  return validate(schemaUpdateContact, req.body, next);
+};
